a9ed554174f73df2d059c0037dd712b81c17cd32
Alrik He <alrik_he@hotmail.com>
2024-04-10 19:09:59 +0200

    feedbackl

--- assignments/Assignment 1 Hampus J/scripts/script.js
+++ assignments/Assignment 1 Hampus J/scripts/script.js
@@ -1,13 +1,15 @@
 
 function log() {
     console.log('Divide');
+    if(isNaN(bar/foo)){
+        alert("You can't divide us!");
+    }
 }
 
 let foo 
 foo = 0
 let bar 
 bar = 0
-
 
 //Ge mig lösningen här tack. Blev halvt knäpp.
 
