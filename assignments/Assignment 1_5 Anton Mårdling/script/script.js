b5f61546a40e55327f03e238c32b95eec7be6d78
Anton <anton__99@hotmail.se>
2024-04-10 22:33:24 +0200

    Assignment 1_5 Anton Mårdling

---
+++ assignments/Assignment 1_5 Anton Mårdling/script/script.js
@@ -0,0 +1,19 @@
+let vip=true
+let age=17
+
+
+
+if (age>=18 && vip==false){
+    alert ("Acces granted") 
+     }
+     if (age<18 && vip==false)
+     alert ("Acces denied")
+     if (age<18 && vip==true)
+     alert ("welcome in my best VIP customer :)")
+
+     
+     
+     
+
+
+     
