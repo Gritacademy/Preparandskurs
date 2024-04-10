de4a799590b4688e145d8e972c753325634a5b5e
Andrea21xd <andreaspin@hotmail.com>
2024-04-10 08:53:02 +0200

    Assignment 1 Andreas

---
+++ assignments/Assignment 1 - Andreas Pintoiu/scripts/script.js
@@ -0,0 +1,19 @@
+let foo
+foo = "0"
+console.log(foo)
+
+let bar
+bar = "0"
+console.log(bar)
+
+
+let result = foo / bar;
+
+if (!isNaN(result)) {
+    console.log(result)
+} else {
+    console.log("You can't divide us!");
+}
+
+
+
