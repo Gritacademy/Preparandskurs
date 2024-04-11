8a256d3f824f9d18e51de4a449f3b880300f03ac
HampusJa <166538006+HampusJa@users.noreply.github.com>
2024-04-11 20:15:53 +0200

    Assigntment 1_5 Hampus J

---
+++ assignments/Assignment 1_5 Hampus J/script.js
@@ -0,0 +1,40 @@
+let age = 17;
+let vip = true;
+
+if (vip == true) {
+  alert("Welcome in my best VIP customer :)");
+}
+if (age >= 18) {
+  alert("Access granted");
+} else {
+  alert("Access denied");
+}
+
+
+
+
+
+
+
+// Fick ingen ordning på det här tyvärr. Om du har tid så visa gärna hur det ska gå till.
+
+/*
+let entrBtn = document.getElementById("entrBtn")
+let age = document.getElementById("Age").value;
+let vip = document.getElementById("ViP").value;
+
+
+
+entrBtn.addEventListener("click")
+
+if (vip == true) {
+  alert("Welcome in my best VIP customer :)");
+}
+if (age >= 18) {
+  alert("Access granted");
+} else {
+  alert("Access denied");
+}
+
+
+*/
