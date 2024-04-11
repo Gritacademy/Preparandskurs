6083e319178ca23d9f9b8ef3ea8eacc2392d85ff
glblg <gianlucalabruna@gmail.com>
2024-04-11 15:11:35 +0200

    Assignment1_5 Gianluca La Bruna

---
+++ assignments/Assignment1_5 Gianluca La Bruna/script.js
@@ -0,0 +1,14 @@
+let vip = true;
+let age = 17;
+
+if (age >= 18) {
+  alert("Access granted");
+} else {
+  alert("Access denied");
+}
+
+if (vip) {
+  alert("Welcome in my best VIP customer :)");
+}
+
+
