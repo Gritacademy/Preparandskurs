a32b536365bb93575c3a48da7dbd6623d79ea3f0
andreitamass <166515040+andreitamass@users.noreply.github.com>
2024-04-11 22:29:49 +0200

    Add files via upload

---
+++ assignments/Assignment 1_5 Andrei/script.js
@@ -0,0 +1,14 @@
+let age = 17;
+let vip = true;
+
+if (age >= 18) {
+    console.log("Access granted");
+} else {
+    console.log("Access denied");    
+}
+
+if (vip == true) {
+    console.log("Welcome my favorite VIP");
+} else {
+    console.log("Access denied");    
+}
