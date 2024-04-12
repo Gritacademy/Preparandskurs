ef336bbb15c906d659e2d3f966d7ff75504e8aa0
Alrik He <alrik_he@hotmail.com>
2024-04-12 12:44:35 +0200

    feedback

--- assignments/assignment 1_5 Mortaza  Haghghi Manie/scripts.js
+++ assignments/assignment 1_5 Mortaza  Haghghi Manie/scripts.js
@@ -1,29 +1,39 @@
-let age;
-let vip;
+let age = 17
+let vip = true
 
-alertbox = "Access granted";
-alertbox1 = "Access denied";
-alertbox2 = "Welcome in my best VIP customer :)";
 
-age = 17;
-vip = true;
+
+alertbox = "Access granted"
+alertbox1 = "Access denied"
+alertbox2 = "Welcome in my best VIP customer :)"
+
+
+
 //checkbox = vip;
 
 function checked() {
-  if (age >= 18) {
-    alert(alertbox);
-  }
-  if (age < 18 && vip == true) {
-    alert(alertbox2);
-  }
+// flytta koden
 }
 function enterBtn() {
-    if (age >= 18) {
-        alert(alertbox);
+   /*  if (age >= 18) {
+        alert("Access granted")
       } else if (age < 18) {
-        alert(alertbox1);
+        alert("Access denied")
       }
 
+ */
+      age = document.getElementById("age").value // läser från input sedan tilldelar
+      vip = document.getElementById("checked").checked 
+      console.log(age,vip)
+      if (vip == true) {
+        alert("Welcome in my best VIP customer :)")
+      }
+      else if (age >= 18) {
+        alert("Access granted")
+      }
+      else  {
+        alert("Access denied")
+      }
 }
 
 
