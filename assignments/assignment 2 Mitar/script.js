7be867320a1f6fd43b0e45d6003b8bd101cf909c
mitar98 <166335752+mitar98@users.noreply.github.com>
2024-04-12 10:05:03 +0200

    Add files via upload

---
+++ assignments/assignment 2 Mitar/script.js
@@ -0,0 +1,10 @@
+let vip = true;
+let age = 17;
+
+if (vip == true) {
+  alert("Welcome in my best VIP customer :)");
+} else if (age >= 18) {
+  alert("Acces granted");
+} else {
+  alert("Acces denied");
+}
