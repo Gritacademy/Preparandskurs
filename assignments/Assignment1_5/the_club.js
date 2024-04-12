9b1057bf0f7b787e4d79a14fcff6bc358ae4c66f
Thiineee <166622948+Thiineee@users.noreply.github.com>
2024-04-12 19:13:24 +0200

    Add files via upload

---
+++ assignments/Assignment1_5/the_club.js
@@ -0,0 +1,13 @@
+let vip = true;
+let age = 17;
+
+if (vip==true) {
+    alert("Welcome in my best VIP customer");
+}
+else if (age>=18) {
+    alert("Access granted"); 
+}
+else (age<=17) {
+    alert("Access denied");
+}
+
