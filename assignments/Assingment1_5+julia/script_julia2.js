09d6752daed967ebb15fb8cb135f47ac55a903d1
Julia <166433478+Julleulle@users.noreply.github.com>
2024-04-14 22:33:17 +0200

    Add files via upload

---
+++ assignments/Assingment1_5+julia/script_julia2.js
@@ -0,0 +1,17 @@
+
+
+
+ 
+
+
+let age = 17;
+let vip = true;
+
+
+   if (age >= 18) {
+    console.log("Access granted");
+  } else if (vip) {
+    console.log("Welcome in my best VIP customer 😁 ");
+  } else {
+    console.log("Access denide");
+  }
