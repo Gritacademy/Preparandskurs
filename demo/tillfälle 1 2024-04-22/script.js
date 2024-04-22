866edbe2d009696a5c51b30ced8dbb31fd80c449
Alrik He <alrik_he@hotmail.com>
2024-04-22 15:54:05 +0200

    demo t1

---
+++ demo/tillfälle 1 2024-04-22/script.js
@@ -0,0 +1,37 @@
+/*
+alert(123)
+document.write(456)
+*/
+//console.log(789); console.log(789)
+
+// 1 ) variabler
+// let namn 
+// namn = värde
+
+let alrik // deklarerar variabeln alrik /skapar variablen som heter alrik
+
+alrik = 5 // tilldela variabeln alrik med värdet 5 /lägger in värdet nummer 5
+
+alrik = "hej" // tilldela med texten "hej"
+
+
+let foo = 10 // deklarera + tilldela 
+
+console.log(alrik) // loggar ut på consolen
+console.log(foo) // loggar ut på consolen
+
+console.log(alrik + foo)
+//1+1 // calkylation
+//"hej" + "san" //concatination
+
+
+console.log( (2 + 2) * 3)
+console.log( "hej" + (1 + 1) )
+console.log(  1 + 1 + "hej"  )
+// P parentess
+// E exponent
+// M Multiplication
+// D Divition
+// A Addition 
+// S sutraction
+
