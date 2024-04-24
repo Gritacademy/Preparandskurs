6bbf603e9b14a09fb99760544a1f9f3af756e520
Hannaein <167784003+Hannaein@users.noreply.github.com>
2024-04-24 12:22:39 +0200

    assignment 1

---
+++ assignments/Assignment_ 1_Hanna/script/scripts.js
@@ -0,0 +1,17 @@
+let foo;
+foo = 0;
+console.log(foo)
+
+let bar;
+bar = 0
+console.log(bar)
+
+let sum = (foo / bar);
+
+if (bar == 0) {
+    console.log("You can't divide us!")
+}   else {
+        console.log("sum")
+}
+
+
