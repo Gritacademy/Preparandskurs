68415e5699ff38c3c53d2f69fabfb0ff9ea98371
Hannaein <167784003+Hannaein@users.noreply.github.com>
2024-04-25 18:48:48 +0200

    assignment1_5

---
+++ assignments/Assignment1_5_Hanna/script/club.js
@@ -0,0 +1,15 @@
+//Deklarera variablerna och tilldela dem med värde vip och age
+let vip = true;
+let age = 17;
+
+//Jämför med om användaren är vip och om användarens ålder
+if (vip == true) {
+    alert("Welcome in my best VIP custumer :)");
+    if (age >= 18){
+         alert("Access granted")
+     }
+     else {
+          alert("Access denied")
+     }
+    
+ }
