66dd11e69b3c201a799ba7bf2dca2868ec3b76a5
Emelee <167675877+Emelizzard@users.noreply.github.com>
2024-04-25 15:44:40 +0200

    Add files via upload

---
+++ assignments/assigment 1_5 Emelie Lindahl/script/script.js
@@ -0,0 +1,26 @@
+
+let vip = true; 
+let age = 17;
+
+ console.log("Vad skriva här?");
+
+
+if (vip==true) {
+    alert("access granted")
+    console.log("viptrue")
+};
+
+if (age>=18) {
+    alert("access granted")
+    console.log("true")
+} else {
+    alert("access denied")
+    console.log("false")
+    
+};
+
+if (vip==true && age>=18 && age<=18) {
+    alert("Welcome in my best VIP customer")
+    console.log("true")
+};
+
