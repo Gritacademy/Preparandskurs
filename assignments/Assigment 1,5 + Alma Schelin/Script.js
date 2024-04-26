ffed719fcf32c85abeca29a78aa303296ee3502f
AlmaSchelin <166640599+AlmaSchelin@users.noreply.github.com>
2024-04-26 09:22:01 +0200

    Assigment 1,5

---
+++ assignments/Assigment 1,5 + Alma Schelin/Script.js
@@ -0,0 +1,17 @@
+let age = 16;
+let vip = true;
+
+if(vip==true){
+    console.log("Welcome in my best VIP customer :)");
+    alert("Welcome in my best VIP customer :)")
+}else{
+    console.log("Not VIP");
+    if (age >= 18){
+        console.log("Access granted");
+        alert("Access granted");
+    }else{
+        console.log("access denied");
+        alert("Access denied");
+    }
+}
+    
