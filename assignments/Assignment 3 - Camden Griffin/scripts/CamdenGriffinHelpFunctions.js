ff4aa8312ad4def9d0aad4b8975e9d61849a0155
camdengriffin <130249171+camdengriffin@users.noreply.github.com>
2024-04-29 15:00:16 +0200

    Add files via upload

---
+++ assignments/Assignment 3 - Camden Griffin/scripts/CamdenGriffinHelpFunctions.js
@@ -0,0 +1,16 @@
+document.getElementById("resultBtn").addEventListener("click", () => {
+    const character = document.getElementById("character").value;
+    const text = document.getElementById("text").value;
+
+    const result = charRemove(character, text);
+    document.getElementById("result").textContent = "Result: " + result;
+});
+
+function charRemove(character, text) {
+    if (typeof character !== "string" || typeof text !== "string") {
+        console.error("Wrong datatype");
+        return;
+    }
+    const result = text.split(character).join("");
+    return result;
+}
