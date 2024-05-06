7399a8ea207a652a4ca93da90effda252e765e26
Alrik He <alrik_he@hotmail.com>
2024-05-06 21:23:13 +0200

    T1 kväll

---
+++ demo/tillfälle 1 2024-05-06 kväll/script.js
@@ -0,0 +1,38 @@
+/*
+alert(123)
+document.write(456) 
+*/
+
+//console.log(111); console.log(222) 
+
+
+//1) variablar
+
+let box2 //Deklarera/skapa en variabel som heter box
+
+box2 = 5 // Tilldela/lägger in numret 5 i variabeln box
+
+box2 = "Tjenare" // tilldelar variabeln med "hej"
+
+let alrik = 10 // Deklarera + tilldelar 
+
+console.log( 1 + 1 ) //kalkylation
+console.log("hej" + "san") // konkatination
+console.log( "Stenyxegatan" + 6) // konkatinerar
+
+console.log( (2 + 2) * 3)
+               //kon   kon
+console.log(  "hej" + (1 + 1) )
+
+console.log(   1 + 1 + "hej")
+
+console.log( box2 + (alrik +alrik) )
+
+
+// PEMDAS
+// P Paraentess
+// E Exponent
+// M multiplikation
+// D Divition
+// A Addition
+// S subtraktion
