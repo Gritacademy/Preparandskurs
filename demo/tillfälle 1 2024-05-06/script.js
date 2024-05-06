0fedb793e41072e3d98e9c636fbafe1d5f7cc436
Alrik He <alrik_he@hotmail.com>
2024-05-06 12:02:05 +0200

    Add files via upload

---
+++ demo/tillfälle 1 2024-05-06/script.js
@@ -0,0 +1,34 @@
+/*
+alert(123)
+document.write(456)
+console.log(789)  
+*/
+
+//console.log(111)   // console.log(222)  
+
+// 1) Variaber 
+// behållare med namn 
+
+let x  // deklarera variablen x 
+
+x = 5  // tilldelar variabeln x med värdet 5
+
+x = "hej" // tilldelar det med texten "hej"
+
+let bar = 10 // deklaration + tilldelning
+console.log( x )
+console.log( (2 + 2) * 3 ) //kalkylation
+
+console.log( "hej" + "san"); //konkatination
+console.log( "hej" + (1 + 1) );
+console.log(  1 + 1 + "hej" );
+console.log( x + (bar + bar));
+// PEMDAS
+// P parentess
+// E Exponent 
+// M Multiplication
+// D Divition
+// A Addition
+// S subtraktion
+
+
