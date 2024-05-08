5c47183570d8fa5b9f9ef88aa82b0e397baeed3e
Alrik He <alrik_he@hotmail.com>
2024-05-08 18:32:00 +0200

    feedback

--- assignments/Assignment1- Matti Walli/Scripts/javascripts.js
+++ assignments/Assignment1- Matti Walli/Scripts/javascripts.js
@@ -1,6 +1,15 @@
-var foo = "You cant";
-var bar = "Divide us!";
+/* var foo = 'You cant'
+var bar = 'Divide us!'
 // foo = 0 ; bar = 0   ska det vara
-console.log(foo); 
-console.log(bar); 
+console.log(foo)
+console.log(bar) */
 
+function btnPressed() {
+    let foo = 0, bar = 0
+    let produkt = foo/bar
+    // foo = 0 ; bar = 0   ska det vara
+    if( isNaN(produkt))
+        console.log("you cant divide us!!!")
+    else 
+        console.log(produkt);
+}
