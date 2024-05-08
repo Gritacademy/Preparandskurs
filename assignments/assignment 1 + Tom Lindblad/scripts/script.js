1ab3c1b4c41acf3431b4a1db07d2fb76ceac29e3
Alrik He <alrik_he@hotmail.com>
2024-05-08 10:32:34 +0200

    feedback

--- assignments/assignment 1 + Tom Lindblad/scripts/script.js
+++ assignments/assignment 1 + Tom Lindblad/scripts/script.js
@@ -1,21 +1,16 @@
-let foo = 0;
-let bar = 0;
-let sum = null;
+let foo = 0
+let bar = 0 // assign till 0
 
-sum = (foo / bar);
+/* let sum = null; */
 
-if (isNaN(sum) || sum <= -0.01)
-    {
-        alert ("You cant divide us!");
+let sum = foo / bar
+
+function divide() { //formatera + flyttade koder in till funktionen för att knappen kör denna
+    if (isNaN(sum) || sum <= -0.01) {
+        // är sum Nan eller sum är mindre än -0.1
+        alert('You cant divide us!')
+    } else {
+        console.log(sum)
     }
-
-else
-    {
-        console.log (sum); 
-    }
-
-function divide()
-    {
-        console.log ("divide");
-    }
-
+    console.log('divide')
+}
