3e4fee6789b715e796c01627f62029b547255902
Alrik He <alrik_he@hotmail.com>
2024-05-08 23:05:41 +0200

    alla koder

---
+++ demo/tillfälle 2 2024-05-08 kväll/script.js
@@ -0,0 +1,53 @@
+//3) datatyper
+
+let x
+console.log(typeof false) //
+// number
+x = 105 // integer int, heltal
+x = 1.5 // float f, decimaltal
+
+// String
+x = 'hej' // String str, strängar /text
+x = '&' // character char ,  karaktär / bokstav
+
+// boolean
+x = true // boolean bool , boolean
+// 0 1 //on/off
+// String + String = String //konkatination
+// String + number = String //konkatination
+// String + boolean = String //konkatination
+
+// number + number = number //kalkylation
+// number + boolean = number //kalkylation
+
+// boolean + boolean = number //kalkylation
+
+/* let firstname = 'Matti',
+    surname = 'Walli',
+   age = 10
+document.write(" <button style='background-color:red;' >" + firstname + ' ' + surname + ' is ' + age + ' years old. </button>')
+ */
+//4) if satser
+// condition ger altid ut booleans 
+// ==  jämnför sidorna om de är samma värde
+// === jämnför + datatypen att det är samma
+// < mindre än
+// > större än 
+// <= mindre än eller jämnför 
+// >= större än eller jämnför
+// !=  inte likamed
+// !== inte likamed + datatypen att det är samma
+let age = 50
+let vip = false
+// && och 
+// || and (altgr + <)
+if (age >= 20 && vip == true) {
+    console.log('Får köpa i systembolaget 1')
+} else {
+    console.log('Får INTE köpa i systembolaget 1')
+}
+
+
+
+console.log("Vidare...")
+
