131cd7246bcc63c53e441c9b2d0852b37b456f16
TomLindblad <168995337+TomLindblad@users.noreply.github.com>
2024-05-08 10:17:06 +0200

    Assignment 1

---
+++ assignments/assignment 1 + Tom Lindblad/scripts/script.js
@@ -0,0 +1,21 @@
+let foo = 0;
+let bar = 0;
+let sum = null;
+
+sum = (foo / bar);
+
+if (isNaN(sum) || sum <= -0.01)
+    {
+        alert ("You cant divide us!");
+    }
+
+else
+    {
+        console.log (sum); 
+    }
+
+function divide()
+    {
+        console.log ("divide");
+    }
+
