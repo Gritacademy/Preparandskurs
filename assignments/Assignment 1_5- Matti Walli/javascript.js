70a9e60c0832a07e9e917acf5b364d6040def30f
mattiwalli <mattiwalli_2000@hotmail.com>
2024-05-09 12:27:11 +0200

    Add files via upload

---
+++ assignments/Assignment 1_5- Matti Walli/javascript.js
@@ -0,0 +1,25 @@
+let vip = true
+let age = 17;
+
+if (age >= 17 && vip == true)
+    {console.log('Access Granted')}
+
+else {console.log('Access Denied')}
+
+
+if (age >= 17) {
+    console.log(true);
+}
+else {
+    console.log(false);
+}
+
+if (vip) {
+    console.log("Welcome in my best VIP customer");
+} else 
+    
+    if (age >= 17) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
