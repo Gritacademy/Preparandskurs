4165e90797bdb26ec10704e56c18d32d64e08d9a
Niklas-Smith <niklas_smith@hotmail.com>
2024-05-26 19:47:02 +0200

    Add files via upload

---
+++ assignments/Assignment 1_5 Niklas Smith/script.js
@@ -0,0 +1,27 @@
+let vip = true
+let age = 17
+
+
+/*if (vip==true||age>=18)
+console.log("Access granted");
+else 
+console.log("Acces denied");
+
+if (age>=18)
+    console.log();
+else
+console.log(); */
+
+
+
+/*if (vip==true||age>=18)
+    alert('Access granted');
+    else alert
+    ('Access denied'); */
+
+
+   /* if (vip == true) alert ('Welcome in my best VIP customer') 
+        else 
+        if (age >=18)
+        alert ('Access granted')
+        else alert ('Access denied') */
