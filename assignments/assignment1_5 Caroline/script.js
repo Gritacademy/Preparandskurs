17bed83661fbbce320836b678227e4c4c986ee0d
Carflo86 <caroline.flotow@gmail.com>
2024-05-27 00:55:42 +0200

    Add files via upload

---
+++ assignments/assignment1_5 Caroline/script.js
@@ -0,0 +1,13 @@
+let vip = true
+let age = 17
+
+if (age >=18 || vip == true)
+    console.log ("access granted")
+else
+console.log ("access denied")
+
+
+if (vip == true)
+    console.log("welcome my best VIP customer:)")
+else 
+ console.log ("access denied")
