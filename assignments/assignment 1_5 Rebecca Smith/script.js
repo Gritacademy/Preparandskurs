5009b97ae386921fad4ed9d47ae6496b0277b00f
Rebexian87 <rebecca.smith@hotmail.se>
2024-05-27 03:33:16 +0200

    Add files via upload

---
+++ assignments/assignment 1_5 Rebecca Smith/script.js
@@ -0,0 +1,12 @@
+
+let VIP = false
+let age = 17
+
+
+/*
+    let vip=document.getElementById('inputVip').checked
+    let age = document.getElementById('inputVip').valueAsNumber
+        
+    if (vip == true ||age>=18) alert ('Access granted') 
+    
+    else alert ('Access denied')" */
