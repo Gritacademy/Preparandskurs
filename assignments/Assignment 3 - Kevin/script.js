a92ee91491e634fb7839dadcc0d0f7a5d5f45ef4
Alrik He <alrik_he@hotmail.com>
2024-05-31 08:31:25 +0200

    feedback

--- assignments/Assignment 3 - Kevin/script.js
+++ assignments/Assignment 3 - Kevin/script.js
@@ -1,8 +1,19 @@
 // Assignment 3a
 
-function charRemove(text = "This code sucks", character = "s") {
-    newText = text.split(character).join("");
-    console.log(newText)
+function charRemove(character = "s",text = "This code sucks"
+) {
+
+/* let resultat=""
+for (let i = 0; i < text.length; i++)
+  if(text[i]!=character)
+      resultat+=text[i]
+console.log(resultat); */
+
+console.log(text.replaceAll(character,""))
+
+ // console.log(text.split(character).toString());
+ //   newText = text.split(character).join("");
+ //   console.log(newText)
 }
 
 charRemove();
@@ -11,12 +22,22 @@ charRemove();
 
 // Assignement 3b
 
-let array = ["Gustav", true];
+//let array = ["Gustav", true];
 let cat_info = {
   name: "Gustav",
   alive: true,
 };
 
 function catName() {
-  alert(array[0]);
+  //alert(array[0]);
+/*  if( Math.random() < 0.5 ) 
+   alert("the cats name is: "+ cat_info.name +" and its alive");
+ else alert("the cats name is: "+ cat_info.name +" and its dead"); */
+
+ catName.alive = Math.random() < 0.5
+ alert("the cats name is: "+ cat_info.name 
+ +( (catName.alive)? " and its alive" : " and its dead") );
+
 }
+
+
