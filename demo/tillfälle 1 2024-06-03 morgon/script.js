4dda9055c8e13ed9650fd53a55a87756964cd1e2
Alrik He <alrik_he@hotmail.com>
2024-06-03 12:03:36 +0200

    t1

---
+++ demo/tillfälle 1 2024-06-03 morgon/script.js
@@ -0,0 +1,44 @@
+/*
+alert(123)
+document.write(456)
+console.log(789)
+*/
+
+/* 
+
+Block Kommentar
+där man skriver beskrivningar
+*/
+
+//
+
+
+let foo  // skapat/deklarerat en variabeln foo 
+
+foo = 5  // lägger värdet nummer 5 till varabeln foo 
+
+foo = 'hej' // string / text / "  '  orange 
+
+let bar = 10 // deklarera & tilldelar variabeln bar med nummer 10
+
+
+console.log( 3 + 2 * 2 ) // kalkylation
+// P arentess
+// E xponent
+// M ultiplikation
+// D ivition
+// A ddition
+// S ubtracktion
+
+
+console.log("hej"+" världen" ) //konkatination
+
+console.log( "hej" + 1 + 1);
+console.log(  1+1 +"hej" );
+
+
+console.log(foo)
+
+console.log(bar)
+
+console.log( bar + bar+ foo + bar + bar+ bar+ bar);
