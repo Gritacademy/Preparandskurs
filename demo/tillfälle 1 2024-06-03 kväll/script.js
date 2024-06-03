81c1428abd7a2a019f43fbd1c7195e4360da4a3d
Alrik He <alrik_he@hotmail.com>
2024-06-03 22:04:06 +0200

    t4 kväll

---
+++ demo/tillfälle 1 2024-06-03 kväll/script.js
@@ -0,0 +1,29 @@
+/*
+
+alert(123)
+document.write(456)
+console.log(789)
+
+*/
+//const är read only variabel
+
+let foo = 599.99 // deklarera variablen foo & tilldelar den med nummer 5
+let bar = "hej"
+foo = "Timearchitect" // tilldelar med stringen "hej"
+
+console.log( (2 + 2) * 3 ); //matte regler
+// P arentess
+// E xponent
+// M ultiplikation
+// D ivition
+// A ddition
+// S ubtraktion
+
+console.log("hej" + 1 + 1);    // konkatination
+console.log(  1 + 1 + "hej");    // konkatination
+
+console.log( 1 + 1 )        // kalkylation
+console.log("hej" + "san" ) // konkatination
+
+console.log(foo+bar)
+
