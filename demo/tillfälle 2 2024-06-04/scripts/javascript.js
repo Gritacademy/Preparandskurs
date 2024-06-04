44790710729cb618703262ec5e67c190456ae800
Alrik He <alrik_he@hotmail.com>
2024-06-04 20:45:01 +0200

    t2

---
+++ demo/tillfälle 2 2024-06-04/scripts/javascript.js
@@ -0,0 +1,37 @@
+let foo = 0
+let bar = 0
+console.log(foo / bar)
+// shift + alt + f = dokument formatering
+
+
+
+
+// 4) if satser 
+
+// i parentess så kan ni har en regel, condition
+
+// om det är sant i parentessen så kör den koden under
+// vid falskt så skippar den den första
+// == jämnför
+// == jämnför + datatyp samma
+// < mindre än
+// > mindre än 
+// <= mindre än eller likamed
+// >= mindre än eller likamed
+// != olikt
+let age = 10 
+let vip = true
+
+if (  age >= 18 ) {
+    console.log("VUXEN")
+} else { // alternativ kan man ha else för falskt
+    console.log("BARN")
+}
+
+if (  vip == true ) {
+    console.log("KÄNDIS")
+} else { // alternativ kan man ha else för falskt
+    console.log("VANLIG PERSON")
+}
+
+
