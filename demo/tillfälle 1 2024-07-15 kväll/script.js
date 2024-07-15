29099951393597f1d7e73f2d25d443d60917d249
Alrik He <alrik_he@hotmail.com>
2024-07-15 19:46:25 +0200

    clear

---
+++ demo/tillfälle 1 2024-07-15 kväll/script.js
@@ -0,0 +1,39 @@
+//alert(123)
+//document.write(456)
+//console.log(789)
+
+//1 variabler
+
+let box1 = 5 //skapat en variabel box1 
+
+// skapa foo
+let foo  // deklarera variabel foo
+
+// lägg in 5 till foo
+foo = 5 // tilldela variabeln foo med siffran 5
+
+foo = 10 // skriver över variabeln
+
+foo = "hej"
+
+foo = box1 // vad kommer båda innehålla?
+
+foo = (2+2)*3     // kalkylation
+
+//Parentess 
+//Exponent
+//Multiplication
+//Divition
+//Addition
+//Subtraktion
+
+foo = "hej" + "san" + "!?" // konkatination
+
+foo = "hej" + 1 + 1  // "hej11" konkatination
+
+
+
+foo =  1 + 1 + "hej"  // "2hej"konkatination
+foo = "hej" + (1 + 1)  // "hej11" konkatination
+
+console.log( foo + box1 );
---
+++ demo/tillfälle 1 2024-07-15/script.js
@@ -0,0 +1,16 @@
+alert(123)
+document.write(456)
+console.log(789)
+
+
+
+let box1 = 5
+
+box1 = 10
+
+box1 = 15
+
+document.write(box1)
+
+// ctrl + plus zoom in
+//
