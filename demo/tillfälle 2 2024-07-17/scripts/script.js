e4a53a0eeaed34749b951904c2812a52797caf17
Alrik He <alrik_he@hotmail.com>
2024-07-17 13:00:23 +0200

    Add files via upload

---
+++ demo/tillfälle 2 2024-07-17/scripts/script.js
@@ -0,0 +1,64 @@
+let x
+console.log(123)
+console.log('hej')
+console.log(true)
+console.log(undefined)
+console.log(x)
+
+// Datatypen
+
+//number
+x = 100 // int,integer/ heltal
+x = 111.5 // f, float/ decimaltal
+
+//string
+x = '&' // char,character / karaktär
+x = 'hej' // str,string / sträng
+
+// boolean
+x = true // bool,boolean/boolean
+
+x = undefined // empty/tomt
+
+//string + string  = string   //konkatination
+//string + number  = string   //konkatination
+//string + boolean = string   //konkatination
+//string + undefined = string //konkatination
+
+//number + number = number    //kalkylation
+//number + boolean = number   //kalkylation
+
+//boolean + boolean = number  //kalkylation
+
+// true / on / 1
+// false / off / 0
+
+// 4 If satser
+
+// alt + ctrl + 7  =  {
+
+// Condition
+// == jämnför
+// < mindre än
+// > större än
+// <= mindre än eller lika med
+// === jämnför + datatypen
+
+
+let age = 10
+let vip = true
+
+if ( age >= 20 ) {
+    console.log(' MYNDIG!!')
+} else { // else är optional, under if alltid
+    console.log(' BARN!!')
+}
+
+
+if ( vip == true ) {
+    console.log(' kändis!!')
+} else { // else är optional, under if alltid
+    console.log(' ple!!')
+}
+
+
