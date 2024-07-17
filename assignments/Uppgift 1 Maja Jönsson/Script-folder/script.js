c4640e30306836da9d92b99b897c98dcebeb95eb
Alrik He <alrik_he@hotmail.com>
2024-07-17 14:04:30 +0200

    feedback

--- assignments/Uppgift 1 Maja Jönsson/Script-folder/script.js
+++ assignments/Uppgift 1 Maja Jönsson/Script-folder/script.js
@@ -1,5 +1,9 @@
-alert("You can't divide us!")
-//console.log   onödig kod
-let foo = 0;
-let bar = 0;
-console.log(foo/bar)
+document.getElementsByTagName("button")[0].addEventListener("click", (e)=>{
+
+    alert("You can't divide us!")
+    //console.log   onödig kod
+    let foo = 0;
+    let bar = 0;
+    console.log(foo/bar)
+
+}  );
