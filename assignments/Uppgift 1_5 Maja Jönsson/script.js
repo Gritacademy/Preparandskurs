28122f7046fdb0d30c9aa312666370ec69258831
Majsan393 <maja00@hotmail.se>
2024-07-17 16:34:42 +0200

    Add files via upload

---
+++ assignments/Uppgift 1_5 Maja Jönsson/script.js
@@ -0,0 +1,26 @@
+let vip = false;
+
+const myAge = document.getElementById("myAge");
+const enterBtn = document.getElementById("enterBtn");
+const ageElement = document.getElementById("ageElement");
+const myVip = document.getElementById("myVip");
+const vipElement = document.getElementById("vipElement")
+
+
+enterBtn.onclick = function(){
+
+    age = myAge.value;
+    age = Number(age);
+
+    if(myVip.checked){
+        ageElement.numberContent = alert("Welcome in my best VIP customer :)");
+    }
+    else if (age >= 18){
+            ageElement.numberContent = alert("Acess granted");
+    }
+    else{
+        ageElement.numberContent = alert("Acess denied");
+    }
+}
+
+
