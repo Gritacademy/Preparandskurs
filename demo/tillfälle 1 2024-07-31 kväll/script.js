7ebb3ee2cb9a536cbb81d11735c4404d54de4a51
Alrik He <alrik_he@hotmail.com>
2024-07-31 21:06:05 +0200

    t1

---
+++ demo/tillfälle 1 2024-07-31 kväll/script.js
@@ -0,0 +1,38 @@
+// alert(123)
+/*
+document.write(456)
+console.log("ALRIK ÄR COOL")
+*/
+
+// 1 variabler
+
+
+let foo  //skapa en variabel foo
+
+foo = 5 // lägger in nummer 5 i foo
+
+foo = "hej" // lägger in texten "hej" till foo
+
+foo = 10
+
+console.log( foo )
+
+let bar = 5  
+// deklarera/declare a variabel
+// tilldela/assign number 5 to bar
+
+console.log( foo + bar )
+console.log( (2 + 2) * 3 )
+
+
+
+console.log(  "hej" + "san"  ) // konkatination/concatination
+console.log(  "hej" + (1 + 1) )
+console.log(  2 + "hej" )
+//P arentess
+//E xponent
+//M ultiplikation
+//D ivition
+//A ddition
+//S ubtraktion
+
