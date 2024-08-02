ab51e1c68732fe3630c7165f7b11824bab462969
Alrik He <alrik_he@hotmail.com>
2024-08-02 20:13:12 +0200

    demo 2

---
+++ demo/tillfälle 2 2024-08-02 kväll/script.js
@@ -0,0 +1,91 @@
+//github
+
+//assignment
+
+// 2 datatyp
+
+let x 
+
+//  numbers
+x = 520      //  int , integer, long / heltal
+x = 50.00   //  f , float, double /  decimaltal
+
+// string
+x = "bok"  //  Str , String / Sträng
+x = '#'    //   char ,  character / karaktär 
+
+// boolean
+x = false   //   bool ,boolean / boolean 
+
+// undefined
+x = undefined // undefined(null) / empty
+
+// string + string = string // concatination
+// string + number = string // concatination
+// string + boolean = string // concatination
+
+// number + number = number // calculation
+// number + boolean = number // calculation
+
+// boolean + boolean = number // calculation
+
+console.log(typeof 5);
+
+
+
+
+
+// 4 if satser
+// (condition)   true/false
+//  == jämnför
+//  <  mindre än
+//  > större än
+//  <=  mindre än eller likamed
+//  >=  större än eller likamed
+//  === jämnför + samma datatype
+
+
+//club
+let age = 32
+let vip = true
+
+if ( age >= 18) {
+    console.log("vuxen")
+}
+else {
+    console.log("BARN")
+    if ( vip == true) {
+        console.log("du är en kändis")
+    }
+    else {
+        console.log("du är en pleb")
+    }
+}
+
+if (age >= 18) 
+    console.log('vuxen')
+else if (vip == true) 
+    console.log('du är en kändis')
+else if (vip == true) 
+    console.log('du är en kändis')
+else if (vip == true) 
+    console.log('du är en kändis')
+else 
+    console.log('du är en pleb')
+
+
+
+// && and och
+// || or  eller
+
+if (age >= 18 || vip == true) {
+    console.log("YEA")
+}
+else {
+    console.log("NOO")
+}
+
+
+
+
+console.log("vidare...")
