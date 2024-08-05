b9638d5a00d4b711d40b569bf40575a564befe9d
leondenengelsen <leon_den_engelsen@live.nl>
2024-08-05 10:17:21 +0200

    Assignment 1_5 Leon den Engelsen
    Tried the optionals but need some more help.

---
+++ assignments/assignment 1_5 leon den engelsen/script.js
@@ -0,0 +1,16 @@
+let vip = true;
+let age = 17;
+
+//  let vip = addEventListener????
+//  let age = addEventListener????
+
+if (vip == true || age >= 17) {
+  console.log("access granted");
+  alert("ACCESS GRANTED 🎉🎉🎉");
+
+  if (vip == true) {
+    console.log("Welcome in, my best V.I.P. friend!");
+  }
+} else {
+  console.log("access denied");
+}
