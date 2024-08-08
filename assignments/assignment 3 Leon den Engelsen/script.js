b1be68ef4a977e8b232322da4e90509a36112ae1
leondenengelsen <leon_den_engelsen@live.nl>
2024-08-08 16:37:02 +0200

    assignment 3 Leon den Engelsen
    assignment 3 Leon den Engelsen

---
+++ assignments/assignment 3 Leon den Engelsen/script.js
@@ -0,0 +1,12 @@
+
+
+let cat = {
+    alive:true, 
+    name:"Gustav",
+}
+
+function showCatName(){
+    alert("The cat's name: " + cat.name);
+    console.log ("The cat's name: " + cat.name);
+}
+
