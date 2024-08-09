85a5181bd0a85ba4a4d442bbe50cb74bf0c18372
Alrik He <alrik_he@hotmail.com>
2024-08-09 18:04:44 +0200

    feedback

--- assignments/assignment 3 Leon den Engelsen/script.js
+++ assignments/assignment 3 Leon den Engelsen/script.js
@@ -1,9 +1,16 @@
 let cat = {
-  alive: true,
-  name: "Gustav",
-};
+    alive: true,
+    name: 'Gustav',
+}
 
 function showCatName() {
-  alert("The cat's name: " + cat.name);
-  console.log("The cat's name: " + cat.name);
+    //alert("The cat's name: " + cat.name)
+    console.log("The cat's name: " + cat.name)
+
+    cat.alive = Math.random() > 0.5
+
+    if (cat.alive)
+       alert("The cat's name: " + cat.name + 'and its alive!!')
+    else 
+       alert("The cat's name: " + cat.name + 'and its dead!!')
 }
