2b4ccd8e7b40c56ffdcfdfa9e84b2adb79b6feb6
Alrik He <alrik_he@hotmail.com>
2024-08-26 20:10:15 +0200

    demo t1

--- assignments/assignment 3 Leon den Engelsen/leondenengelsen_Helpfunctions.js
+++ assignments/assignment 3 Leon den Engelsen/leondenengelsen_Helpfunctions.js
@@ -1,4 +1,19 @@
-function charRemove (char, text){
-
+function charRemove(char, text) {
+   // let result = ''
+    //for (let i = 0; i < text.length; i++) 
+       // text= text.replace(char,"")
+        //if (char != text.charAt(i)) 
+         //   result += text.charAt(i)
+    console.log(text.replaceAll(char,""))
 }
 
+function charRemoveSheroq(char, text) {
+    if (typeof text != 'string') console.log('wrong datatype')
+    else if (char.length < 1) console.log('no letters in the text')
+    else {
+        console.log('Character: ', char)
+        console.log('Text: ', text)
+    }
+}
+
+charRemove('A', '123')
