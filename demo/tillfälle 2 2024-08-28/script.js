d650374782df3002753e56864f2c7e0e6a0f3a18
Alrik He <alrik_he@hotmail.com>
2024-08-28 22:22:48 +0200

    t2

---
+++ demo/tillfälle 2 2024-08-28/script.js
@@ -0,0 +1,46 @@
+// 2 datatyper
+
+let x
+let y
+
+// number
+x = -5 // heltal/ integer, int
+x = 0.5 // decimaltal/ float, f , double
+
+// string
+x = 'hejsan' //  sträng/ String, str
+x = '$' // karaktär/ charater , char
+
+//boolean
+x = false //boolean/ boolean , bool
+
+//undefined
+x = undefined // tomt/undefined
+
+// string + string = string  //concatination
+// string + number = string  //concatination
+// string + boolean = string //concatination
+
+// number + number = number  //calculation
+// number + boolean = number  //calculation
+
+// boolean + boolean = number //calculation
+
+// 3 if satser
+// conditions är koder som ger sant/falsk tillslut
+// == jämnförelse vänster & höger
+// ===  jämnförelse + datatyp
+// < mindre än
+// <= mindre än + lika med
+// > större än
+// >= större än + lika med
+
+let age = 32
+
+if(age >= 20) {
+    console.log('Ni får köpa alkohol') //sant kod
+}else {
+    console.log("Ni är för ung"); //falsk kod
+}
+
+
