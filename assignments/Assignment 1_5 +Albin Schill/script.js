bf26e2773b08162dfaffe74601dd1bb679597f11
Abbe-03 <albin.schill@hotmail.se>
2024-08-30 18:10:13 +0200

    Add files via upload

---
+++ assignments/Assignment 1_5 +Albin Schill/script.js
@@ -0,0 +1,13 @@
+let age = 16
+let vip = true
+
+if(age >= 18) {
+    console.log('Welcome to Da CLub')
+}
+else {
+    if (vip == true) {
+        console.log("Welcome mister importain")    
+    }   
+    if (vip== false) {
+    console.log("Get of of here ya F@#?ing kid!");
+}}
