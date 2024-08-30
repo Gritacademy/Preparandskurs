3a2a2acbb86a17703ad6161ad4ba20be0ba4b20a
Alrik He <alrik_he@hotmail.com>
2024-08-30 18:22:22 +0200

    feedback

--- assignments/Assignment 1_5 +Albin Schill/script.js
+++ assignments/Assignment 1_5 +Albin Schill/script.js
@@ -1,7 +1,7 @@
-let age = 16
+let age = 10
 let vip = true
 
-if(age >= 18) {
+/* if(age >= 18) {
     console.log('Welcome to Da CLub')
 }
 else {
@@ -10,4 +10,23 @@ else {
     }   
     if (vip== false) {
     console.log("Get of of here ya F@#?ing kid!");
-}}
+}} */
+
+if (vip) 
+    console.log('Welcome mister importain')
+else if (age >= 18) 
+    console.log('Welcome to Da CLub')
+else 
+    console.log('Get of of here ya F@#?ing kid!')
+
+{ { { } } }
+
+if (vip == true) {
+    console.log('Welcome mister importain')
+} else {
+    if (age >= 18) {
+        console.log('Welcome to Da CLub')
+    } else {
+        console.log('Get of of here ya F@#?ing kid!')
+    }
+}
