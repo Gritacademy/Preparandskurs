1d10a53e2327a5df7f082e1b80e9b136d993571a
elijnan <elinajohansson@hotmail.com>
2024-09-03 20:52:49 +0200

    Add files via upload

---
+++ assignments/assignment 1 + Elina/scripts/script.js
@@ -0,0 +1,37 @@
+alert("You cant divide us!")
+
+foo = 0
+bar = 0
+
+
+console.log
+
+/*h1{  
+    /*
+    färg på texten
+
+    color:red;
+    
+
+    font-size: 50px;
+    margin-left: 150px;
+    
+} 
+
+button {
+    background-color: green;
+}
+
+input{
+    border-color: purple;
+    border-width: 5px;
+    border-style: dotted;
+    padding: 20px;
+    margin:20px;
+}
+    
+
+
+
+
+-"märkligt nog upptäckte jag att man kan skriva in bokstaven e i input boxarna trots att de ej är nummer...... but why?"*/
