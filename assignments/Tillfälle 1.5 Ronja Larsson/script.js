23a78c01b4e2362d7dd1304f6a4f64dae6352d6a
tofzz <ronjalarsson1@gmail.com>
2024-09-23 17:50:08 +0200

    Add files via upload

---
+++ assignments/Tillfälle 1.5 Ronja Larsson/script.js
@@ -0,0 +1,21 @@
+
+ageInput = document.getElementById("age");
+vipCheckbox = document.getElementById("vip");
+enterBtn = document.getElementById("enterBtn");
+
+enterBtn.addEventListener("click", function() {
+    let age = parseInt(ageInput.value);
+    let vip = vipCheckbox.checked;
+
+    if (vip && age >= 18) { 
+alert("Welcome by best VIP customer :)"); 
+} else if (age >= 18) { 
+    alert("Access granted");
+     } else { 
+        alert("Access denied"); 
+    }
+ });
+
+ let vip = false;
+ let age = 17;
+
