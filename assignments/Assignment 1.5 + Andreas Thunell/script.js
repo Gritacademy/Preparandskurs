1385fcd979f0b2ecea25534387aad29f9906f8b5
Andreas Thunell <andreas.thunell@gritacademy.se>
2024-10-27 12:53:21 +0100

    Assignment 1.5 + Andreas Thunell

---
+++ assignments/Assignment 1.5 + Andreas Thunell/script.js
@@ -0,0 +1,16 @@
+ let age = 18
+ let vip = true
+
+      
+
+        if (vip == true) {
+            console.log ('Welcome very important person')
+        } else {
+                if (age >= 18) {
+                    console.log ('Welcome to Catalina Whinemixer club')
+
+                    } else {
+                        console.log ("Have a walk and come back when you are a little more drunk, kiddo")
+                         }
+                        
+                        }
