c4ecb71b9ed18390bd93c5bf93dd85a93611f392
Alrik He <alrik_he@hotmail.com>
2025-05-05 20:17:57 +0200

    t1

---
+++ demo/tillfälle 1 2025-05-05/script.js
@@ -0,0 +1,48 @@
+
+/*
+alert(123)          //alertbox pausar hemsidan, ingen kod körs om inte du trycker på ok
+document.write(456) //skriver på index.html
+console.log(789)    //skriver ut i consolen i inspektorn
+*/
+
+
+
+/*
+document.title = "HEJSAN!!!"
+document.body.style.backgroundColor="red"
+*/
+
+
+// Variabler 1)
+
+let foo = 5 //skapar en behållare med 5 i
+
+foo = 10
+foo = 20
+foo = 1 + 1
+foo = 2 + 3 * 2
+foo = 2 * (3 + 2)
+foo = "hej"    //text har citat tecken orange
+foo = "hej" + 1
+foo =  1 + "hej"
+foo =  1 + 1 + "hej"
+foo =  "hej" + (1 + 1)
+
+//foo =  "hej1" + 1 
+//foo = "hej" + "san"
+// P arentess ()
+// E xponent ^
+// M ultiplikation * 
+// D ivition /
+// A ddition +
+// S ubtraktion -
+console.log(foo);
+
+let bar // deklarera en variabel som heter bar
+
+console.log(bar  );
+
+bar = "ALrik"
+console.log( bar + " är bäst" );
+
+
