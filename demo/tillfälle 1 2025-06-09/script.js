1af2263036c46aebf1d9d100daaa5decb7d1a95e
Alrik He <alrik_he@hotmail.com>
2025-06-09 20:03:57 +0200

    t1

---
+++ demo/tillfälle 1 2025-06-09/script.js
@@ -0,0 +1,34 @@
+/*
+alert(123)
+document.write(456)
+console.log(789)
+*/
+
+
+
+// 1) variabler , behållare för information
+
+let foo  //skapar foo
+foo = 5  // lägger in siffran 5
+foo = 5 + 5 // kalkylation alla operatorer körs innan den läggs in
+console.log(foo) // skickar ut till konsolen
+
+
+let x = 12 //skapar x lägger in siffran 12
+x = 99     // skriver över en
+x = "hejsan"  // text
+x = "yxgatan 5" // text
+x = "5"
+x = "5" + "5" // konkatination
+x = 1 + 1 + "hej" 
+x = "hej" + (1 + 1)
+x = (2 + 3) * 2  //multiplikation 
+console.log(x);
+console.log(x + foo);
+
+//P arentess
+//E xponent
+//M ultplikation
+//D ivition
+//A ddition
+//S ubtraktion
