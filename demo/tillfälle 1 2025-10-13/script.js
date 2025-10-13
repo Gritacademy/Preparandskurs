c346c0e9e20ecbfd72b2ebfcbec4289376eedfb1
Alrik He <alrik_he@hotmail.com>
2025-10-13 20:35:40 +0200

    T1

---
+++ demo/tillfälle 1 2025-10-13/script.js
@@ -0,0 +1,18 @@
+/*
+alert(123)
+document.write(456)
+console.log(789)
+*/
+
+
+// 1) Variabler , behållare
+// innehåller 1 sak
+
+let foo = 5 // variablen heter foo 
+foo = 10 //sätter in 10
+foo = "hej" 
+
+let score //deklarera är att skapa en variabel
+
+console.log(score)
+console.log(1+1)
