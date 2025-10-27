7af4487f48d6b9dd42d0c62bbfe7231f600fe271
Alrik He <alrik_he@hotmail.com>
2025-10-27 22:34:23 +0100

    t1

---
+++ demo/tillfälle 1 2025-10-27/script.js
@@ -0,0 +1,27 @@
+// alert(123)
+
+/*
+alert("YOU HAVE WON 1000000$ CONGRATZ!!!!!")
+prompt("WRITE YOU EMAIL ADRESS HERE: ;)")
+document.write(456)
+*/
+console.log(789)
+
+// 1) Variable
+// behållare
+let foo   //skapa variablen som heter foo 
+foo = 5   //lägg in siffran 5 till variabeln foo
+foo = 10  //lägg in siffran 5 till variabeln foo
+
+
+let bar = "hej2"  //deklarera/declare bar lägg in text "hej"
+bar = 'YO5' // tilldela / assign
+
+let x 
+console.log( x )
+console.log( foo )
+console.log( bar )
+
+//deklarera en variabel som heter greeting & tilldela den med texten "HEJSAN"
+
+let greeting ="HEJSAN"
