c14bac9c7f8edfa5e2e1b03d0dee4d4b47499ab3
Alrik He <alrik_he@hotmail.com>
2025-11-10 22:19:51 +0100

    t1

---
+++ demo/tillfälle 1 2025-11-10/script.js
@@ -0,0 +1,33 @@
+
+
+//alert( 123 )
+
+/* 
+document.write( 456 )
+document.write( "hejsan jag heter Alrik He" )
+console.log( 789 ) 
+*/
+
+console.log( "detta är ett test" )
+
+
+// 1) variabler, behållare med data/info i
+
+let x   //skapar en variabel som heter alrik
+x = 5   //lägger in ett värde nummer 5 i variabeln alrik
+
+console.log( x )
+
+x = 10  //tilldelar/assign alrik med numret 10
+
+let bar = "Hejsan" // deklarera/declare variabeln bar
+                  // & tilldelar den texten "Hejsan"
+bar = "tjenare"  // 
+
+// uttryck
+console.log(     1  +  1    );
+console.log(  "hej" + "san"   );
+console.log( x +" .....   "+ bar );
+
+
+
