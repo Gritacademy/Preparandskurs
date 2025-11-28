d5391e985917fdf61ae2d01622bb6975bdb02368
Alrik He <alrik_he@hotmail.com>
2025-11-28 20:52:13 +0100

    t1

---
+++ demo/tillfälle 1 2025-11-28/script.js
@@ -0,0 +1,24 @@
+//alert( 123 )
+document.writeln( 456 )
+//console.log ( 789 )
+
+/* 
+  Koden är av Alrik He
+  från Gritacademy prepkurs
+  tillfälle 1
+*/
+console.log ("hejsan" )
+
+
+// 1) Variabler 
+// behållare
+
+let foo  //skapar variabeln som heter foo
+        //deklarera/declare variabeln foo
+foo = 5  // sätter in siffran 5 til variabeln foo
+        // tilldela/assign numer 5 till variabeln foo
+foo = 10
+
+console.log( foo )
+
+foo = 50
