7118e74c247642d2fdad43784823d2a72024f915
Alrik He <alrik_he@hotmail.com>
2026-03-30 21:01:17 +0200

    Add files via upload

---
+++ demo/tillfälle 1 2026-03-30/script.js
@@ -0,0 +1,30 @@
+
+// kommentarer basics
+// Alrik är cool !!
+
+//alert(123)
+/*
+document.writeln(456)
+console.log(789) // console.log(789)
+console.log(1000000000)
+*/
+//console.log("hejsan")
+
+// skapa en behållare som heter box med nummer 5
+let box = 5 
+box = 10   //ändra vi till att den innehåller 10 istället
+box = "hello world"
+
+console.log(box)
+
+box = "Alrik är cool"
+
+console.log(box)
+
+
+let foo  // deklarera en variabel som heter foo
+foo = 88 // tilldelar 88 till variabeln foo
+
+console.log( 1+1 )
+console.log( 2*2 )
+console.log( foo+22 )
