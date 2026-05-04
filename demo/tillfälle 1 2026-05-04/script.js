3c7804d61e02aee2c3c1e1ac92151f53d3e41414
Alrik He <alrik_he@hotmail.com>
2026-05-05 00:21:38 +0200

    t1

---
+++ demo/tillfälle 1 2026-05-04/script.js
@@ -0,0 +1,39 @@
+/*
+alert(123)
+document.write(456)
+*/
+
+//console.log(789)
+
+//variabler 1)
+// hejsan jag heter Alrik 
+
+let foo  //deklarera / skapa en variabel som heter foo
+
+foo = 5  //tilldelat variabeln foo med siffran 5 /lägger in 5
+
+foo = 10
+
+console.log(foo)
+
+let bar = 10 // deklarera + tilldela 
+/*
+console.log( 1 + 1 )
+console.log( 2 * 2 )
+console.log( "hejsan jag heter Alrik" )
+*/
+console.log( "hej" + "san" )
+console.log( (2 + 3) * 2 )
+console.log( "hej" + 1 )
+console.log( "hej" + (1 + 1) )
+console.log(  1 + 1 + "hej" )
+
+console.log(bar + foo)
+
+
+//P arentess
+//E xponent
+//M ultiplikation
+//D ivition
+//A ddition
+//S ubtraktion
