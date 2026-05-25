e32fc5007ea0edce7f948d5d9e206a15a8926637
Alrik He <alrik_he@hotmail.com>
2026-05-25 20:00:50 +0200

    t1

---
+++ demo/tillfälle 1 2026-05-25/script.js
@@ -0,0 +1,34 @@
+/* 
+alert(123)
+document.write(456)
+console.log(789)
+ */
+
+let foo  // skapa variabeln, deklarera en variabel som heter foo
+foo = 5 // lägger in 5 , tilldela värdet nummer 5
+
+console.log(foo);
+console.log(999);
+
+foo = 10 // ordning / skriver över
+
+let bar = "hej" //deklaration av variabeln bar med tilldelningen stringen "hej"
+
+console.log(bar);
+
+console.log( (2 + 2) * 3 );
+
+console.log(  1 + 1 + "hej");
+
+console.log( "hej" +( 1 + 1) );
+
+
+console.log(  2 / 2 )
+console.log(  foo + bar )
+
+//P arentess
+//E xponent
+//M ultiplikation
+//D ivition
+//A ddition
+//S ubtraktion
