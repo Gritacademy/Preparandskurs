7a611206377b38b51377e5b9f58c2733f30187f0
Alrik He <alrik_he@hotmail.com>
2026-05-27 23:48:05 +0200

    t2

---
+++ demo/tillfälle 2 2026-05-27/script/script.js
@@ -0,0 +1,69 @@
+/* alert(123)
+let foo = 1 */
+
+//  datatyper 2)
+
+let x //deklarerad variabel x
+
+//number js  .
+x = 15 // heltal , integer ; int , Long , 3422224L
+x = 0.25 // decimaltal , float; 0.25f , double , D
+
+//string
+x = "#" // character ; char
+x = "text dsf ew wef w" // String ; Str
+x = `text dsf 
+sfd sd s` // String ; Str
+
+
+//boolean    false => 0,  true => 1
+x = true //boolean ; bool
+
+//String + String  = String    //konkatinering concat
+// "hej" + "hej"   = "hejhej"
+
+//String + number  = String    //konkatinering concatnation
+// "hej" + 1       = "hej1"
+
+//String + boolean = String    //konkatinering concatnation
+// "hej" + true    = "hejtrue"
+
+// number + number =  number   //Kalkylerar  calculation
+//   1    +    1   =  2
+
+// number + boolean = number   //Kalkylerar  calculation
+//   1    +   true  = 2
+
+// boolean + boolean = number  //Kalkylerar  calculation
+//  true   +  false  =  1
+
+// conditions / if satser 3)
+
+let age = document.getElementById("age").valueAsNumber
+
+//condition
+// ==  jämnför båda sidorna om värdet är samma
+// >  större än
+// >= större än eller lika med
+// <  mindre än
+// <= mindre än eller lika med
+// != inte lika med
+// ! not , inte
+// === lika med och samma datatyp
+
+// &&  och and
+// ||  eller or
+/* 
+let businessOwner = document.getElementById("owner").checked
+
+if (businessOwner) {
+    document.getElementById("age").disabled = true
+    console.log("YES du är en vuxen person du äger en restaurang bro!!")
+} else if (age >= 20) {
+    console.log("YES du är en vuxen person")
+} else {
+    console.log("No du är ett barn")
+}
+
+
+ */
