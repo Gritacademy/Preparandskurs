fbf5fdb64b2a238cbd263dccde8393ec2d6ddb32
Alrik He <alrik_he@hotmail.com>
2026-06-22 20:14:55 +0200

    t1

---
+++ demo/tillfälle 1 2026-06-22/script.js
@@ -0,0 +1,30 @@
+/*
+alert(123)
+document.write(456)
+*/
+
+console.log(789); // console.log(789) 
+
+
+// detta är symbolerna för kommentarer //
+
+
+// variabler 1)
+
+// behållarer med data
+
+//deklaration
+let foo //  deklarera variabeln foo
+
+//tilldelning
+foo = 88 //tilldela variabeln foo med numret 88
+
+foo = 'hej' //tilldela variabeln foo med texten "hej"
+
+let bar = 5 //deklaration + tilldelning
+
+console.log(bar);
+
+let point = 0
+
+
